21c3de80c162bb131efbc542ab69928468231a00
wxj <139439432@qq.com>
2017-09-11 23:22:45 +0800

    0911

--- phone/nav.js
+++ phone/nav.js
@@ -3,8 +3,8 @@ $(function(){
     var nav = '<div class="logo"><img src="../img/logo.png" alt=""></div>'+
               '<div class="menu" id="menu">'+
               '  <img src="../img/menu.png" alt="">'+
-              '</div>'+
-              '<div class="menu-wrap">'+
+              '</div>';
+    var menu = '<div class="menu-wrap">'+
               '  <ul class="menu-cont">'+
               '    <li class="menu-item"><a href="../index/index.html">首页</a></li>'+
               '    <li class="menu-item"><a href="../thin/index.html">瘦身</a></li>'+
@@ -12,6 +12,8 @@ $(function(){
               '    <li class="menu-item"><a href="../about/index.html">关于我们</a></li>'+
               '  </ul>'+
               '</div>'
+              
+    $("body").append(menu)
     $("#nav").append(nav)
     $("#menu").click(function () {
       $(".menu-wrap").toggle()
